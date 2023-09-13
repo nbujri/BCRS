aed35c0d5c6a66d5d3dc41e74cef627710c68ca2
Ngi Bujri <nbujri7@gmail.com>
2023-09-13 13:21:55 -0400

    add routing for sign in page

--- src/app/security/security-routing.module.ts
+++ src/app/security/security-routing.module.ts
@@ -8,17 +8,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SecurityComponent } from './security.component';
+import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [
   {
     path: '',
     component: SecurityComponent,
-    title: 'BCRS: Security'
-  }
+    title: 'BCRS: Security',
+    // child routes
+    children: [
+      {
+        path: 'signin',
+        component: SigninComponent,
+        title: 'BCRS: Sign In',
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SecurityRoutingModule { }
+export class SecurityRoutingModule {}
