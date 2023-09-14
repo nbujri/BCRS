e7a29e6dc4f7ce3260cb6ed6cf8de2c639bda8d3
Ngi Bujri <nbujri7@gmail.com>
2023-09-14 16:41:45 -0400

    genereate user-list component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { UserListComponent } from './users/user-list/user-list.component';
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    UserListComponent
   ],
   imports: [
     CommonModule,
