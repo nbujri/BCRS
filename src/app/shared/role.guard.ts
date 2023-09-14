1a482a04f19e8db5e66caad46f3d445256367496
mwalker06 <81312304+mwalker06@users.noreply.github.com>
2023-09-13 19:34:10 -0500

    added content to roleguard

--- src/app/shared/role.guard.ts
+++ src/app/shared/role.guard.ts
@@ -7,8 +7,29 @@ Source: Professor Krasso, Angular.io */
 
 // imports
 import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 // roleGuard function
 export const roleGuard: CanActivateFn = (route, state) => {
-  return true;
+  const cookie = inject(CookieService);
+
+  // get session user
+  let sessionUser = cookie.get('session_user');
+
+  console.log(sessionUser);
+
+  // if session user is not logged in, redirect to signin page
+  if (!sessionUser) {
+    console.log('You must be logged in to access this page.');
+    const router = inject(Router);
+    router.navigate(['/session/signin'], { queryParams: { returnUrl: state.url } })
+    return false;
+  }
+
+  // if session user is logged in, check role
+  if sessionUser.role === 'admin' {
+    return false;
+  }
+
 };
