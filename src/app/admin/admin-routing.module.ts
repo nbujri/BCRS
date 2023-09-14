f4683af5a90133f2fff608a8bc971cb3d8d7d6dd
Ngi Bujri <nbujri7@gmail.com>
2023-09-14 16:46:48 -0400

    add routing to user-list component

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,32 @@
+/**
+ * title: admin-routing.module.ts
+ * author: ngi bujri
+ * date: september 14 2023
+ * description: routing for admin module
+ */
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin.component';
+import { UserListComponent } from './users/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      {
+        // path to user-list component (user config page)
+        path: 'users',
+        component: UserListComponent,
+        title: 'BCRS: Users',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
