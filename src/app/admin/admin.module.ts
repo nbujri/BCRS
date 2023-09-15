ac090585faf1a9c81da7e1670d90f635f94edf67
Ngi Bujri <nbujri7@gmail.com>
2023-09-15 14:52:22 -0400

    import modules

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { UserListComponent } from './users/user-list/user-list.component';
 
-
 @NgModule({
-  declarations: [
-    AdminComponent,
-    UserListComponent
-  ],
+  declarations: [AdminComponent, UserListComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
-  ]
+    AdminRoutingModule,
+    HttpClientModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class AdminModule { }
+export class AdminModule {}
