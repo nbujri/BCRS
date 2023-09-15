03ba27fe2efa91acbe2edc87afad4d41830c789a
mwalker06 <81312304+mwalker06@users.noreply.github.com>
2023-09-14 22:05:16 -0500

    started signin.component.ts

--- src/app/security/signin/signin.component.ts
+++ src/app/security/signin/signin.component.ts
@@ -1,10 +1,32 @@
+/* Title: signin.component
+Author: Megan Walker, Ngi Bujri, Caitlynne Johnson
+Date: 09-14-2023
+Description: signin component for BCRS
+Source: Professor Krasso, Angular.io */
+
+
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { ActivatedRoute , Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+import { SecurityService } from '../security.service';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
+
 export class SigninComponent {
+  errorMessage: string;
+  isLoading: boolean = false;
+
+  signinForm = this.fb.group({
+    email: [null, Validators.compose([Validators.required, Validators.email])]
+    password: [null, Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z]+$')])]
+  })
+
+
+
 
 }
