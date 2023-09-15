bfe92cff896805ce85afcaabdfee83bcdc6c997e
Ngi Bujri <nbujri7@gmail.com>
2023-09-15 16:16:52 -0400

    call findAllUsers to return array of users

--- src/app/admin/users/user-list/user-list.component.ts
+++ src/app/admin/users/user-list/user-list.component.ts
@@ -4,14 +4,52 @@ Date: 09-14-2023
 Description: user-list component for BCRS
 Source: Professor Krasso, Angular.io */
 
-
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { User } from '../user';
+import { Colors } from 'chart.js';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent {
+  users: User[]; // store users here
+  successMessage: string; // store success messages here
+  errorMessage: string; // store error messages gere
+  isLoading: boolean; // for loading spinner
 
+  constructor(private userService: UserService) {
+    // initialize variables
+    this.users = [];
+    this.successMessage = '';
+    this.errorMessage = '';
+    this.isLoading = true;
+
+    // return all users
+    this.userService.findAllUsers().subscribe({
+      next: (users: any) => {
+        this.users = users;
+        console.log('users: ', this.users);
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.errorMessage = err.message;
+        console.log(err);
+        this.isLoading = false;
+      },
+      complete: () => {
+        this.isLoading = false;
+      },
+    });
+  }
+
+  // hides messages after 3 seconds
+  hideAlert() {
+    setTimeout(() => {
+      this.successMessage = ''; // reset successMessage
+      this.errorMessage = ''; //reset errorMessage
+    }, 3000);
+  }
 }
