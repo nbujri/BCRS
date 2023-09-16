1b260cf6aea5940e2f1710939893988be12981d1
Ngi Bujri <nbujri7@gmail.com>
2023-09-16 14:37:44 -0400

    add role guard for admin module

--- src/app/shared/role.guard.ts
+++ src/app/shared/role.guard.ts
@@ -4,34 +4,38 @@ Date: 09-13-2023
 Description: role guard for BCRS
 Source: Professor Krasso, Angular.io */
 
-
 // imports
 import { CanActivateFn, Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service'
-import { inject } from '@angular/core'
+import { CookieService } from 'ngx-cookie-service';
+import { inject } from '@angular/core';
 
 // role guard function
 export const roleGuard: CanActivateFn = (route, state) => {
-  const cookie = inject(CookieService)
+  const cookie = inject(CookieService);
 
   // get session user
-  let sessionUser = JSON.parse(cookie.get('session_user'))
+  let sessionUser = JSON.parse(cookie.get('session_user'));
 
-  console.log('Session User:', sessionUser) // for troubleshooting purposes
+  console.log('Session User:', sessionUser); // for troubleshooting purposes
 
   // if no session user, redirect to sign in
   if (!sessionUser) {
-    console.log('You must be logged in to access this page!')
-    const router = inject(Router)
-    router.navigate(['/security/signin'], { queryParams: { returnUrl: state.url } })
-    return false
+    console.log('You must be logged in to access this page!');
+    const router = inject(Router);
+    router.navigate(['/security/signin'], {
+      queryParams: { returnUrl: state.url },
+    });
+    return false;
   }
 
   // if session user is not an admin, redirect to home
   if (sessionUser.role !== 'admin') {
-    return false
+    console.log('you must be an admin to access this page!');
+    const router = inject(Router);
+    router.navigate(['/']);
+    return false;
   }
 
   // if session user is an admin, allow access
-  return true
+  return true;
 };
