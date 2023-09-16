013632cee6c27b074d47c7595ed029d53dd9373b
Ngi Bujri <nbujri7@gmail.com>
2023-09-16 19:52:01 -0400

    import modules

--- src/app/admin/users/user-update/user-update.component.ts
+++ src/app/admin/users/user-update/user-update.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../user.service';
+import { User } from '../user';
+import { UserViewModel } from '../user-view-model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-update',
   templateUrl: './user-update.component.html',
-  styleUrls: ['./user-update.component.css']
+  styleUrls: ['./user-update.component.css'],
 })
-export class UserUpdateComponent {
-
-}
+export class UserUpdateComponent {}
