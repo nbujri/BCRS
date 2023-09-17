3ed96e53470f85892e8973b8a9a4c52200898b92
Ngi Bujri <nbujri7@gmail.com>
2023-09-16 20:35:51 -0400

    add path to user-update component

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -9,6 +9,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { UserListComponent } from './users/user-list/user-list.component';
+import { UserUpdateComponent } from './users/user-update/user-update.component';
 
 const routes: Routes = [
   {
@@ -20,6 +21,12 @@ const routes: Routes = [
         path: 'users',
         component: UserListComponent,
         title: 'BCRS: Users',
+      },
+      {
+        // path to user-update component
+        path: 'user-update',
+        component: UserUpdateComponent,
+        title: 'BCRS: User Update',
       },
     ],
   },
