1f0a6131bf6d12db79069d39336e03f17c90c895
Ngi Bujri <nbujri7@gmail.com>
2023-09-20 18:42:21 -0400

    fix typos

--- server/routes/security.js
+++ server/routes/security.js
@@ -57,8 +57,8 @@ const registerSchema = {
   required: [
     "email",
     "password",
-    "firstname",
-    "lastname",
+    "firstName",
+    "lastName",
     "selectedSecurityQuestion",
   ],
   additionalProperties: false,
