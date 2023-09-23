2f4bbb5c9d97f8d626589ae83b437f30b948827e
mwalker06 <81312304+mwalker06@users.noreply.github.com>
2023-09-22 21:02:42 -0500

    fixed errors

--- src/app/security/reset-password/reset-password.component.ts
+++ src/app/security/reset-password/reset-password.component.ts
@@ -5,60 +5,71 @@ Description: Reset password component
 Source: Professor Krasso, Angular.io
  */
 
+// import statements
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SecurityService } from '../security.service';
-import { ActivatedRoute } from '@angular/router';
 
-
+// component details
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.css']
 })
+
+// export class ResetPasswordComponent implements OnInit
 export class ResetPasswordComponent {
-  errorMessage: string;
-  email: string;
-  isLoading: boolean = false;
+  errorMessage: string
+  email: string
+  isLoading: boolean = false
 
-  // reset password form group for the reset password component
+  // change password form group for the reset password component
   changePasswordForm: FormGroup = this.fb.group({
-    password: [null, Validators.compose([Validators.required, Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$')])],
-  });
+    password: [
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.pattern(
+          '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'
+        ),
+      ]),
+    ],
+  })
 
-  // constructor with form builder, security service, and activated route
+  // constructor with form builder, security service, router, and activated route
   constructor(private fb: FormBuilder, private securityService: SecurityService, private route: ActivatedRoute, private router: Router) {
-    this.email = this.route.snapshot.queryParamMap.get('email') ?? '';
-    this.errorMessage = '';
+    this.email = this.route.snapshot.queryParamMap.get('email') ?? '' // get the email address from the query parameters
+    this.errorMessage = '' // set the error message to an empty string
 
-    //if the email is empty, navigate to the root page
+    // if the email address is not found, redirect to the signin page
     if (!this.email) {
-      console.log('No email provided. Redirecting to root page.');
-      this.router.navigate(['/']);
+      console.log('No email address found')
+      this.router.navigate(['/security/signin'])
     }
   }
 
   // change password function that takes in the form values and calls the security service to change the password
   changePassword() {
-    this.isLoading = true;
+    this.isLoading = true
+    const password = this.changePasswordForm.controls['password'].value
 
-    const password = this.changePasswordForm.controls['password'].value;
-
-    // call the security service to change the password
     this.securityService.changePassword(this.email, password).subscribe({
-      // if successful, navigate to the sign in page
       next: (data) => {
-        console.log(data);
-        this.router.navigate(['/session/signin']);  // navigate to the sign in page
+        console.log(data)
+        this.router.navigate(['/security/signin'])
       },
-      // if unsuccessful, display an error message
+
+      // if the call is unsuccessful, display an error message
       error: (err) => {
-        console.log(err);
+        console.log(err)
         this.errorMessage = err
-        this.isLoading = false;
+        this.isLoading = false
       },
+
+      // if the call is successful, display a message
       complete: () => {
-        this.isLoading = false;
+        this.isLoading = false
       }
     })
   }
--- src/app/security/security.service.ts
+++ src/app/security/security.service.ts
@@ -4,14 +4,14 @@ Date: 09-13-2023
 Description: Security service for BCRS
 Source: Professor Krasso, Angular.io */
 
-// imports for security service
+// imports
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { RegisterViewModel } from './register-view-model';
 import { selectedSecurityQuestionsViewModel } from './selected-security-questions-view-model';
-import { Observable } from 'rxjs';
 
-// security service injectable
+// security service
 @Injectable({
   providedIn: 'root'
 })
@@ -21,7 +21,7 @@ export class SecurityService {
 
   constructor(private http: HttpClient) { }
 
-  // signup function for security service
+  // signin function
   signin(email: string, password: string) {
     return this.http.post('/api/security/signin', {
       email,
@@ -29,28 +29,28 @@ export class SecurityService {
     })
   }
 
-  // register function for security service
-  register(user:RegisterViewModel) {
-    return this.http.post('/api/security/register', {user })
+  // register function
+  register(user: RegisterViewModel) {
+    return this.http.post('/api/security/register', { user })
   }
 
-  // verify email function for security service
+  // find user by email function
   verifyEmail(email: string) {
     return this.http.post('/api/security/verify/users/' + email, {})
   }
 
-  // returns the findSelectedSecurityQuestions function
+  // find selected security questions function
   findSelectedSecurityQuestions(email: string) {
-    return this.http.get('/api/user' + email + '/security-questions')
+    return this.http.get('/api/users/' + email + '/security-questions')
   }
 
-  // returns the verifySecurityQuestions function
+  // verify security questions function
   verifySecurityQuestions(email: string, securityQuestions: selectedSecurityQuestionsViewModel[]): Observable<any> {
-    return this.http.post('/api/security/verify/user/' + email + 'security-questions', {securityQuestions})
+    return this.http.post('/api/security/verify/users/' + email + '/security-questions', { securityQuestions })
   }
 
-  // returns the resetPassword function
-  resetPassword(email: string, password: string): Observable<any> {
-    return this.http.post('/api/security/users/' + email + '/reset-password', {password})
+  // reset password function
+  changePassword(email: string, password: string): Observable<any> {
+    return this.http.post('/api/security/users/' + email + '/reset-password', { password })
   }
 }
