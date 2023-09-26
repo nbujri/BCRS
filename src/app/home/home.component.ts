38237f6fd55b45df72792f6aa0dd040685ed9f53
Ngi Bujri <nbujri7@gmail.com>
2023-09-26 14:25:45 -0400

    get cookie

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,13 +7,21 @@ Source: Professor Krasso, Angular.io */
 // imports statements
 import { Component } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [DatePipe]
+  providers: [DatePipe],
 })
 export class HomeComponent {
   myDate = Date.now();
+  cookie;
+
+  constructor(private cookieService: CookieService) {
+    // get session_user cookie to access user's email
+    this.cookie = JSON.parse(this.cookieService.get('session_user'));
+    console.log('cookie: ', this.cookie);
+  }
 }
