d8b3e37749bf0f629397b75aa0c1e019f8ad84ed
mwalker06 <81312304+mwalker06@users.noreply.github.com>
2023-09-26 21:54:47 -0500

    added route for invoice.js

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ const swaggerUI = require("swagger-ui-express");
 // route files
 const userRoute = require("../server/routes/user");
 const securityRoute = require("../server/routes/security");
+const invoiceRoute = require("../server/routes/invoice");
 
 // Create the Express app
 const app = express();
@@ -29,6 +30,8 @@ app.use("/", express.static(path.join(__dirname, "../dist/bcrs")));
 // api route
 app.use("/api/users", userRoute);
 app.use("/api/security", securityRoute);
+app.use("/api/invoices", invoiceRoute);
+
 
 // swagger router
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
