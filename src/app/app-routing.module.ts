db796c0bb3474106065fee29979ea0d58a670be2
Cait Johnson <caitjohnson@wrightsgate.net>
2023-10-01 16:37:27 -0800

    fixing errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,14 +14,10 @@ import { authGuard } from './shared/auth.guard';
 import { roleGuard } from './shared/role.guard';
 import { EmployeeDirectoryComponent } from './employee-directory/employee-directory.component';
 import { FaqComponent } from './faq/faq.component';
-<<<<<<< HEAD
-import { PurchasesByServiceComponent } from './purchases-by-service/purchases-by-service.component';
-import { InvoiceSummaryComponent } from './invoice-summary/invoice-summary.component';
-=======
 import { ServiceRepairComponent } from './service-repair/service-repair.component';
 import { InvoiceSummaryComponent } from './invoice-summary/invoice-summary.component';
 import { InvoiceListComponent } from './invoice-list/invoice-list.component';
->>>>>>> mwalker-dev
+import { ServiceGraphComponent } from './service-graph/service-graph.component';
 
 // routes array with a path, component, and title for each route in the application (e.g. home, about, contact, etc.)
 const routes: Routes = [
@@ -56,8 +52,8 @@ const routes: Routes = [
         title: 'BCRS: FAQ'
       },
       {
-        path: 'purchases-by-service',
-        component: PurchasesByServiceComponent,
+        path: 'service-graph',
+        component: ServiceGraphComponent,
         title: 'BCRS: Purchases By Service'
       },
       {
